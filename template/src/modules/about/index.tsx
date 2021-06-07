ee4cc206f69bfec3f1cb0005914333fb7c8ad952
Tengfei Xi <xitf@glodon.com>
2021-06-07 17:37:51 +0800

    update

--- template/src/modules/about/index.tsx
+++ template/src/modules/about/index.tsx
@@ -5,16 +5,16 @@ interface Props{
     title?: string;
 }
 
-const MyComponent: React.FC<Props> = function(props){
+const About: React.FC<Props> = function(props){
   const {
     title
   } = props;
 
   return(
     <div className="my-component">
-      <Button>LeafletMap</Button>
+      <Button>About</Button>
     </div>
   )
 }
 
-export default MyComponent;
+export default About;
